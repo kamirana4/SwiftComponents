034d6599071918d9912296cb6430433d854bdb8f
M.Kamran <samerdabbagh@SAMERs-MacBook-Pro.local>
2017-11-22 10:39:29 +0400

    TabBarExample

---
+++ TabBarExample/TabBarExample/ViewControllers/Home/First/FirstViewController.swift
@@ -0,0 +1,23 @@
+//
+//  FirstViewController.swift
+//  TabBarExample
+//
+//  Created by M.Kamran on 11/2/17.
+//  Copyright © 2017 M.Kamran. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
+
